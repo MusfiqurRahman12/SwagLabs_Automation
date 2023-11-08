4b8ba5f63434f23fbce09e7dacc8c3ca15f5f442
MusfiqurRahman12 <143134506+MusfiqurRahman12@users.noreply.github.com>
2023-11-08 06:12:22 +0600

    S.spec.js

--- S.spec.js
+++ S.spec.js
@@ -1,13 +1,4 @@
-// import { test, expect } from '@playwright/test';
 
-// test('test', async ({ page }) => {
-//   await page.goto('https://www.saucedemo.com/v1/');
-//   await page.locator('[data-test="username"]').click();
-//   await page.locator('[data-test="username"]').fill('standard_user');
-//   await page.locator('[data-test="password"]').click();
-//   await page.locator('[data-test="password"]').fill('secret_sauce');
-//   await page.getByRole('button', { name: 'LOGIN' }).click();
-// });
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
